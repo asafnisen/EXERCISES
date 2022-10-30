e56c61f5ff7bb9f30764c06bfbeaa95af5eece59
asaf nisencwajg <nisen1986@gmail.com>
2022-10-30 05:28:55 +0200

    ד

---
+++ spark.py
@@ -0,0 +1,50 @@
+import pyspark
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+
+import pyodbc
+from sql_config import SQL_SERVER_server, SQL_SERVER_database
+
+conn = pyodbc.connect('Driver={SQL Server};'+
+                      'Server={};'.format(SQL_SERVER_server)+
+                      'Database={};'.format(SQL_SERVER_database)+
+                      'Trusted_Connection=yes;')
+
+## all_video_advertisers 
+spark = SparkSession.builder.master("local[1]").appName("SparkByExamples.com").getOrCreate()
+df = spark.read.option('header','true').csv("C:\PYTHON\\all_video_advertisers.CSV")
+df2= df.where("not (_c1 is null and _c2 is null and _c3 is null )") 
+newcolumns = ['Date','Advertiser_Name','Domain','Revenue']
+df2= df2.toDF(*newcolumns)
+# FILTERS 
+#3 אופציות
+#all_video_advertisers = df2.where(~df2.Date.contains('Date')) 
+#או 
+# df2.where(df2['Date'] !='Date').show()
+#או 
+
+all_video_advertisers = df2.where("Date != 'Date'")         # ordering the data
+
+
+
+## Rubicon  
+df1= spark.read.option("multiline","true") \
+      .json(r"C:\PYTHON\Rubicon.json")
+
+df1= df1.withColumn("Advertiser_Name", lit("Rubicon"))
+
+newcolumns = ['Date','Advertiser_Name','Domain','Revenue']
+df_Rubicon = df1.select("Date","Advertiser_Name","Referring Domain","Publisher Net Revenue").toDF(*newcolumns) # ordering the data
+
+
+df_Rubicon.createOrReplaceTempView("temp")
+all_video_advertisers.createOrReplaceTempView("temp2")
+sqlDF = spark.sql(" select ROW_NUMBER() OVER(PARTITION BY Advertiser_Name,Domain ORDER BY Date ASC) ,* from (SELECT * FROM temp union SELECT * FROM temp2) t where Advertiser_Name = 'Rubicon' order by Advertiser_Name,Domain ")
+
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM asaf')
+
+for i in cursor:
+    print(i)
---
+++ sql_config.py
@@ -0,0 +1,8 @@
+"""App configuration constants"""
+SQL_SERVER_server = 'LTH80051170\SQLEXPRESS'
+SQL_SERVER_database = 'TEST_asaf'
+
+MY_SQL_host="localhost",
+MY_SQL_user="root",
+MY_SQL_password="123456",
+MY_SQL_database="stocks"
